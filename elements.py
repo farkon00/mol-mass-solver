62e8743ebde34955fb1daae83545ac0ef9b033b0
farkon00 <sammer2016sammer@gmail.com>
2022-11-09 09:52:56 +0200

    Initial commit

---
+++ elements.py
@@ -0,0 +1,136 @@
+dict_el = {
+    "H" : 1, #HYDROGEN 1
+    "He" : 4, #HELIUM 2 
+
+    "Li" : 7, #LITHIUM 3 
+    "Be" : 9, #BERYLLIUM 4
+    "B" : 11, #BORON 5
+    "C" : 12, #CARBON 6
+    "N" : 14, #NITROGEN 7
+    "O" : 16, #OXYGEN 8
+    "F" : 19, #FLOURINE 9
+    "Ne" : 20, #NEON 10
+
+    "Na" : 23, #SODIUM 11
+    "Mg" : 24, #MAGNESIUM 12
+    "Al" : 27, #ALUMINIUM 13
+    "Si" : 28, #SILICON 14
+    "P" : 31, #PHOSPHORUS 15
+    "S" : 32, #SULFUR 16
+    "Cl" : 35, #CHLORINE 17
+    "Ar" : 40, #ARGON 18
+
+    "K" : 39, #POTTASIUM 19
+    "Ca" : 40, #CALCIUM 20
+    "Sc" : 45, #SKANDIUM 21
+    "Ti" : 48, #TITANIUM 22
+    "V" : 51, #VANADIUM 23
+    "Cr" : 52, #CHROMIUM 24
+    "Mn" : 55, #MANGANESE 25
+    "Fe" : 56, #IRON 26
+    "Co" : 59, #COBALT 27
+    "Ni" : 59, #NICKEL 28
+    "Cu" : 64, #COPPER 29
+    "Zi" : 65, #ZINC 30
+    "Ga" : 70, #GALLIUM 31
+    "Ge" : 73, #GERMANIUM 32
+    "As" : 75, #ARSENIC 33
+    "Se" : 79, #SELENIUM 34
+    "Br" : 80, #BROMINE 35
+    "Kr" : 84, #KRYPTON 36
+
+    "Rb" : 85, #RUBIDIUM 37
+    "Sr" : 88, #STRONTIUM 38
+    "Y" : 89, #YTTRIUM 39
+    "Z" : 91, #ZICRONIUM 40
+    "Nb" : 93, #NOBIUM 41
+    "Mo" : 96, #MOLYBDONUM 42
+    "Tc" : 98, #TECHNETIUM 43
+    "Ru" : 101, #RUTHENIUM 44
+    "Rh" : 103, #RHODIUM 45
+    "Pd" : 106, #PALLADIUM 46
+    "Ag" : 108, #SILVER 47
+    "Cd" : 112, #CADMIUM 48
+    "In" : 115, #INDIUM 49
+    "Sn" : 119, #TIN 50
+    "Sb" : 122, #ANTIMONY 51
+    "Te" : 128, #TELLURIUM 52
+    "I" : 127, #INDINE 53
+    "Xe" : 131, #XENON 54
+    "Cs" : 133, #CAESIUM 55
+    "Ba" : 137, #BARIUM 56
+
+    #LANTHANIDE SERIES
+
+    "La" : 139, #LANTHANUM 57
+    "Ce" : 140, #CERIUM 58
+    "Pr" : 141, #PRASEODYMIUM 59
+    "Nd" : 144, #NEODYMIUM 60
+    "Pm" : 145, #PROMETHIUM 61
+    "Sm" : 150, #SAMARIUM 62
+    "Eu" : 152, #EUROPUM 63
+    "Gd" : 157, #GADOLINIUM 64
+    "Tb" : 159, #TERIBIUM 65
+    "Dy" : 163, #DYSPROSIUM 66
+    "Ho" : 165, #HOLMIUM 67
+    "Er" : 167, #ERBIUM 68
+    "Tm" : 169, #THULIUM 69
+    "Yb" : 173, #YTTERBIUM 70
+    "Lu" : 175, #LUTETIUM 71
+
+
+
+    "Hf" : 179, #HAFNIUM 72
+    "Ta" : 181, #TANTALUM 73
+    "W" : 184, #TUNGSTEN 74
+    "Re" : 186, #RHENIUM 75
+    "Os" : 190, #OSMIUM 76
+    "Ir" : 192, #IRIDIUM 77
+    "Pt" : 196, #PLATINUM 78
+    "Au" : 197, #GOLD 79
+    "Hg" : 201, #MERCURY 80
+    "Tl" : 204, #THALLIUM 81
+    "Pb" : 207, #LEAD 82
+    "Bi" : 209, #BISMUTH 83
+    "Po" : 209, #POLONIUM 84
+    "At" : 210, #ASTATINE 85
+    "Rn" : 222, #RADON 86
+    "Fr" : 223, #FRANCIUM 87
+    "Ra" : 226, #RADIUM 88
+
+    #ACTINIDE SERIES
+
+    "Ac" : 227, #ACTINIUM 89
+    "Th" : 232, #THORIUM 90
+    "Pa" : 231, #PROTACTINIUM 91
+    "U" : 238, #URANIUM 92
+    "Np" : 237, #NEPTUNIUM 93
+    "Pu" : 244, #PLUTONIUM 94
+    "Am" : 243, #AMERICIUM 95
+    "Cm" : 247, #CURIUM 96
+    "Bk" : 247, #BERKELIUM 97
+    "Cf" : 251, #CALIFORNIUM 98
+    "Es" : 252, #EINSTEINIUM 99
+    "Fm" : 257, #FERMIUM 100
+    "Md" : 258, #MENDELEVIUM 101
+    "No" : 259, #NOBELIUM 102
+    "Lr" : 262, #LAWRENCIUM 103
+
+
+
+    "Rf" : 267, #RUTHERFORDIUM 104
+    "Db" : 268, #DUBNIUM 105
+    "Sg" : 271, #SEABORGIUM 106
+    "Bh" : 272, #BOHRIUM 107
+    "Hs" : 270, #HASSIUM 108
+    "Mt" : 276, #MEITNERIUM 109
+    "Ds" : 281, #DARMSTADIUM 110
+    "Rg" : 280, #ROENTGENIUM 111
+    "Cn" : 285, #COPERNICIUM 112
+    "Nh" : 284, #NIHONIO 113
+    "Fl" : 289, #FLEROVIUM 114
+    "Mc" : 289, #MOSCOVIO 115
+    'Lv' : 293, #LIVERMORIUM 116
+    "Ts" : 294, #TENNESINE 117
+    'Og' : 294, #OGANESSON 118
+}
---
+++ main.py
@@ -0,0 +1,124 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Iterator
+
+from elements import dict_el
+
+class TokenKind(Enum):
+    ELEM = auto()
+    NUM = auto()
+    L_BRACK = auto()
+    R_BRACK = auto()
+
+@dataclass
+class Token:
+    kind: TokenKind
+    val: str
+
+def get_generic_tok_type(tok: str) -> TokenKind:
+    if tok.isnumeric():
+        return TokenKind.NUM
+    else:
+        return TokenKind.ELEM
+
+def lex(string: str) -> list[Token]:
+    tokens = []
+    tok = ""
+    for char in string:
+        if char == "(":
+            if tok:
+                tokens.append(Token(get_generic_tok_type(tok), tok))
+            tokens.append(Token(TokenKind.L_BRACK, "("))
+            tok = ""
+        elif char == ")":
+            if tok:
+                tokens.append(Token(get_generic_tok_type(tok), tok))
+            tokens.append(Token(TokenKind.R_BRACK, ")"))
+            tok = ""
+        elif char.isupper() or char.isnumeric():
+            if tok:
+                tokens.append(Token(get_generic_tok_type(tok), tok))
+            tok = char
+        elif char.isspace():
+            if tok:
+                tokens.append(Token(get_generic_tok_type(tok), tok))
+            tok = ""
+        else:
+            tok += char
+    
+    if tok:
+        tokens.append(Token(get_generic_tok_type(tok), tok))
+
+    return tokens
+
+def add_elem(elems: list[tuple[int, str]], elem: tuple[int, str]):
+    elem_index = None
+    for index, e in enumerate(elems):
+        if e[1] == elem[1]:
+            elem_index = index
+    if elem_index is not None:
+        elems[elem_index] = (elem[0] + elems[elem_index][0], elem[1])
+    else:
+        elems.append(elem)
+
+def parse(tokens: Iterator[Token], nested: bool = False) -> list[tuple[int, str]]:
+    elems: list[tuple[int, str]] = []
+    elem = None
+    elem_bracks = None
+
+    for tok in tokens:
+        if tok.kind == TokenKind.ELEM:
+            if elem:
+                add_elem(elems, (1, elem))
+            elem = tok.val
+        elif tok.kind == TokenKind.NUM:
+            if elem:
+                add_elem(elems, (int(tok.val), elem))
+                elem = None
+            elif elem_bracks:
+                for e in elem_bracks:
+                    add_elem(elems, (e[0]*int(tok.val), e[1]))
+                elem_bracks = None
+            else:
+                print("Invalid position of an index")
+                exit(1)
+        elif tok.kind == TokenKind.L_BRACK:
+            elem_bracks = parse(tokens, nested=True)
+        elif tok.kind == TokenKind.R_BRACK:
+            if not nested:
+                print("Bracket wasn't matched")
+                exit(1)
+            break
+    else:
+        if nested:
+            print("Bracket was not closed")
+            exit(1)
+
+    if elem is not None:
+        add_elem(elems, (1, elem))
+
+    if elem_bracks is not None:
+        for e in elem_bracks:
+            add_elem(elems, e)
+
+    return elems
+
+def get_atom_mass(elem: str) -> int:
+    if elem in dict_el:
+        return dict_el[elem]
+    else:
+        print(f"Element \"{elem}\" was not found")
+        exit(1)
+
+def compute_molecular_mass(elems: list[tuple[int, str]]) -> int:
+    return sum(map(
+        lambda elem: get_atom_mass(elem[1]) * elem[0], 
+        elems
+    ))
+
+def main():
+    while True:
+        print(compute_molecular_mass(parse(iter(lex(input())))))
+
+if __name__ == "__main__":
+    main()
